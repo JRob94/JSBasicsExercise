359eb2b336951b06b748a0da687764e9ca0e6fe4
Jordan Roberts <jxavier831@gmail.com>
2021-09-20 15:50:25 -0400

    JS Basics Exercise completed with some Bonus

---
+++ script.js
@@ -0,0 +1,42 @@
+// JSBASICSEXERCISE
+// 1
+console.log(13);
+
+// 2
+/*
+Numbers
+Strings
+Booleans
+Null
+Undefined
+ */
+
+// 3
+/*
+Adding +
+Subtracting -
+Multiplying *
+Division /
+Exponentiation **
+Modulus %
+*/
+
+// 4
+console.log(5%4);
+console.log(1%5);
+
+//  5
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+// 6 
+console.log("Hawk" + "Eye");
+
+// 7
+console.log("Jordan".length + "Roberts".length);
+
+// 8
+console.log("Avengers: Endgame"[16]);
+
+// BONUS
+// 1
+console.log("\\");
